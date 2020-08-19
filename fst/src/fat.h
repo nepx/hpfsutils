b78c95eb710c59f489adf92f46768b649ab0ba25
nepx <23300488+nepx@users.noreply.github.com>
2020-08-19 08:45:35 -0700

    Make the PACKED attribute actually work

--- fst/src/fat.h
+++ fst/src/fat.h
@@ -30,87 +30,87 @@ Boston, MA 02111-1307, USA.  */
 
 typedef struct
 {
-  BYTE name[8+3] PACKED;
-  BYTE attr PACKED;
-  BYTE reserved[8] PACKED;
-  USHORT ea PACKED;             /* Or upper 16 bits of cluster for FAT32 */
-  USHORT time PACKED;
-  USHORT date PACKED;
-  USHORT cluster16 PACKED;
-  ULONG size PACKED;
-} FAT_DIRENT;
+  BYTE name[8+3];
+  BYTE attr;
+  BYTE reserved[8];
+  USHORT ea;             /* Or upper 16 bits of cluster for FAT32 */
+  USHORT time;
+  USHORT date;
+  USHORT cluster16;
+  ULONG size;
+} PACKED FAT_DIRENT;
 
 typedef struct
 {
-  BYTE flag PACKED;
-  USHORT name1[5] PACKED;
-  BYTE attr PACKED;
-  BYTE reserved PACKED;
-  BYTE checksum PACKED;
-  USHORT name2[6] PACKED;
-  USHORT cluster PACKED;
-  USHORT name3[2] PACKED;
-} VFAT_DIRENT;
+  BYTE flag;
+  USHORT name1[5];
+  BYTE attr;
+  BYTE reserved;
+  BYTE checksum;
+  USHORT name2[6];
+  USHORT cluster;
+  USHORT name3[2];
+} PACKED VFAT_DIRENT;
 
 typedef union
 {
   BYTE raw[2048];
   struct
     {
-      BYTE   jump[3] PACKED;
-      BYTE   oem[8] PACKED;
-      USHORT bytes_per_sector PACKED;
-      BYTE   sectors_per_cluster PACKED;
-      USHORT reserved_sectors PACKED;
-      BYTE   fats PACKED;
-      USHORT root_entries PACKED;
-      USHORT sectors PACKED;
-      BYTE   media PACKED;
-      USHORT sectors_per_fat PACKED;
-      USHORT sectors_per_track PACKED;
-      USHORT heads PACKED;
-      USHORT hidden_sectors_lo PACKED;
-      USHORT hidden_sectors_hi PACKED;
-      ULONG  large_sectors PACKED;
+      BYTE   jump[3];
+      BYTE   oem[8];
+      USHORT bytes_per_sector;
+      BYTE   sectors_per_cluster;
+      USHORT reserved_sectors;
+      BYTE   fats;
+      USHORT root_entries;
+      USHORT sectors;
+      BYTE   media;
+      USHORT sectors_per_fat;
+      USHORT sectors_per_track;
+      USHORT heads;
+      USHORT hidden_sectors_lo;
+      USHORT hidden_sectors_hi;
+      ULONG  large_sectors;
       union
       {
         struct
         {
-          BYTE   drive_no PACKED;
-          BYTE   reserved PACKED;
-          BYTE   extended_sig PACKED;
-          ULONG  vol_id PACKED;
-          BYTE   vol_label[11] PACKED;
-          BYTE   vol_type[8] PACKED;
-        } fat;
+          BYTE   drive_no;
+          BYTE   reserved;
+          BYTE   extended_sig;
+          ULONG  vol_id;
+          BYTE   vol_label[11];
+          BYTE   vol_type[8];
+        } PACKED fat;
         struct
         {
-          ULONG  sectors_per_fat PACKED;
-          USHORT flags PACKED;
-          BYTE   version[2] PACKED;
-          ULONG  root_cluster PACKED;
-          USHORT info_sector PACKED;
-          USHORT boot_sector_backup PACKED;
-          BYTE   reserved[12] PACKED;
-        } fat32;
+          ULONG  sectors_per_fat;
+          USHORT flags;
+          BYTE   version[2];
+          ULONG  root_cluster;
+          USHORT info_sector;
+          USHORT boot_sector_backup;
+          BYTE   reserved[12];
+        } PACKED fat32;
       } r;
-    } boot;
+    } PACKED boot;
 #ifdef OS2 /* TODO: define FEALIST etc. for Unix */
   struct
     {
-      BYTE   magic[2] PACKED;	/* "ED" */
-      USHORT unused[15] PACKED;
-      USHORT table[240] PACKED;
-    } ea1;
+      BYTE   magic[2];	/* "ED" */
+      USHORT unused[15];
+      USHORT table[240];
+    } PACKED ea1;
   struct
     {
-      BYTE    magic[2] PACKED;	/* "EA" */
-      USHORT  rel_cluster PACKED;
-      ULONG   need_eas PACKED;
-      BYTE    name[14] PACKED;
-      BYTE    unknown[4] PACKED;
-      FEALIST fealist PACKED;
-    } ea3;
+      BYTE    magic[2];	/* "EA" */
+      USHORT  rel_cluster;
+      ULONG   need_eas;
+      BYTE    name[14];
+      BYTE    unknown[4];
+      FEALIST fealist;
+    } PACKED ea3;
 #endif
   } FAT_SECTOR;
 #pragma pack()
